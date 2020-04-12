ff90251fd2a44f7eecf1318d7a7ca68dfb32ac9d
Zsámár Olivér <xxt07d@gmail.com>
2020-04-12 04:59:57 +0200

    Initial commit

---
+++ app/src/main/java/com/zsami/covid_19stats/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.zsami.covid_19stats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zsami.covid_19stats.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
